abb7d15b8d3ac2bf433aa9e95c32f1adb186c195
juapatral <juapatral@gmail.com>
2019-08-03 17:50:15 -0500

    Updated app

--- app/ui.R
+++ app/ui.R
@@ -18,22 +18,29 @@ fluidPage(
   # crear pestania principal
   
       tabsetPanel( 
-        # inicial
+        
+        # pestania de inicio
         tabPanel("Inicio", includeMarkdown("opening.md")),
         
-        # resumen
+        # pestania de resumen
         tabPanel("Resumen", includeMarkdown("resumen.md")),
         
-        # informe
+        # pestania de informe
         tabPanel("Informe", includeHTML("accidentalidad-2014-2018.html")),
         
-        # aplicacion
+        # pestania de aplicacion
         tabPanel("App",
+                 
+                 # crear barra lateral
                  sidebarPanel(width = 3,
+                              
+                   # seleccion de rango de fechas
                    dateRangeInput('dateRange',
                                   label = 'Ingrese rangos de fechas:',
                                   start = as.Date("2019-01-01"), end = Sys.Date()
                                   ),
+                   
+                   # seleccion de tipo de fechas
                    selectInput(inputId = "tipo_fecha", 
                               label = "Tipo de fecha:",
                               choices = c("Diario" = "dia",
@@ -43,22 +50,30 @@ fluidPage(
                                           "Anual" = "anio"
                                           )
                               ),
+                   
+                   # seleccion de tipo de accidentes
                    selectInput(inputId = "tipo_accidente",
                                label = "Tipo de accidente:",
                                choices = accidentes)
                    ),
+                 
+                 # crear panel principal
                  mainPanel(
+                   
+                   # crear mapa
                    leaflet() %>% 
                      addPolygons(data = barrios, 
                                  color = "black", 
                                  opacity = 0.1, 
                                  fill = T, 
-                                 #fillColor = c("red", "blue", "green"), 
-                                 weight = 2
+                                 fillColor = "grey", 
+                                 weight = 3
                                  ) %>% 
-    addProviderTiles(providers$Wikimedia)
-                 )
+                     addProviderTiles(providers$Wikimedia)
+                   )
                  ),
+        
+        # pestania de video
         tabPanel("Video explicativo", HTML('<iframe width="813" height="457" src="https://www.youtube.com/embed/tAA_yWX8ycQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'))
       )
     )
